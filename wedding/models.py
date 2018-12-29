c1ae97841b7f5568cec0b376c6b2d357087f2c22
Ruben Valseca <rubn.vp@gmail.com>
2018-12-29 15:49:10 +0100

    get posts from db

--- wedding/models.py
+++ wedding/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Attendee(models.Model):
     name = models.CharField(max_length=50)
@@ -13,11 +14,18 @@ class Attendee(models.Model):
         verbose_name_plural = 'Asistentes'
 
 class BlogPost(models.Model):
+    slug = models.SlugField(max_length=30, unique=True)
     title = models.CharField(max_length=30, verbose_name="Título")
-    slug = models.CharField(max_length=20, verbose_name="URL")
+    subtitle = models.CharField(max_length=100, verbose_name="Subtítulo", null=True)
+    image = models.ImageField(null=True, upload_to="wedding/static/images/")
     content = models.TextField(max_length=500, verbose_name="Contenido")
     created_at = models.DateField(auto_now=True)
     publication_date = models.DateField(
         verbose_name="Fecha de publicación automática",
         null=True,
     )
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(BlogPost, self).save(*args, **kwargs)
